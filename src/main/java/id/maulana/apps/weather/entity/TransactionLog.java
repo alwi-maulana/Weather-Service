8fcd623788b43979b5b85eccbb71ffd77fc617f0
Alwi Maulana <alwi.maulana@ifg-life.id>
2025-04-12 01:08:18 +0700

    ADD: transaction log entity

---
+++ src/main/java/id/maulana/apps/weather/entity/TransactionLog.java
@@ -0,0 +1,9 @@
+//package id.maulana.apps.weather.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "bmi_calculation_history", schema = "underwriting_schema", catalog = "automation")
+//public class TransactionLog {
+//}
